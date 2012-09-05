c41898931d41d78a9de39e850940cd0007c96da4
tgloeggl <tgloeggl@uos.de>
2012-09-05 09:10:11 +0200

    Achievements: add new achievement, thanks to tleilax

---
+++ achievements/AchievementAssociationGold.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * AchievementAssocationGold.php - Members of the Stud.IP e.V. get this Achievement
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      Jan-Hendrik Willms <tleilax+studip@gmail.com>
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+ */
+class AchievementAssociationGold implements Achievement {
+
+    public static function hasMetRequirements($user_id) {           
+        $stmt = DBManager::get()->prepare("SELECT COUNT(*)
+            FROM seminar_user 
+            WHERE seminar_id = 'a9fdca48b9a972fbb12408afdc17c9d5'
+                AND user_id = ?");
+        $stmt->execute(array($user_id));
+
+        return $stmt->fetchColumn() > 0;
+    }
+
+    public static function getProgress($user_id) {
+        return _('Leider bist du kein Vereins-Mitglied.');
+    }
+
+    public static function getTitle() {
+        return _("Du bist ein Vereins-Mitglied.");
+    }
+
+    public static function getDescription() {
+        return _('Diese Trophäe erhalten nur Vereins-Mitglieder.');
+    }
+}
--- achievements/AchievementBuddyBronze.php
+++ achievements/AchievementBuddyBronze.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * AchievementBuddyBronze.php - Achievement for the first buddy
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      Till Glöggler <studip@inspace.de>
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GPL version 3
+ * @category    Stud.IP
+ */
 class AchievementBuddyBronze implements Achievement {
 
     public static function hasMetRequirements($user_id) {
